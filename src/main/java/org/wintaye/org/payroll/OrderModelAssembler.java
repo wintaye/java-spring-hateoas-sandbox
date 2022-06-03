0c6242ce7c8e5347bec3a9d896a62014e05c6426
wintaye <41543902+wintaye@users.noreply.github.com>
2022-06-02 23:07:47 -0500

    API working
    working, end of day

---
+++ src/main/java/org/wintaye/org/payroll/OrderModelAssembler.java
@@ -0,0 +1,17 @@
+//package org.wintaye.org.payroll;
+//
+//import org.springframework.hateoas.EntityModel;
+//import org.springframework.hateoas.server.RepresentationModelAssembler;
+//import org.springframework.stereotype.Component;
+//import org.wintaye.org.payroll.models.Order;
+//
+//@Component
+//public class OrderModelAssembler implements RepresentationModelAssembler<Order, EntityModel<Order>> {
+//
+//    @Override
+//    public EntityModel<Order> toModel(Order order){
+//
+//
+//    }
+//
+//}
--- src/main/java/org/wintaye/org/payroll/controllers/OrderController.java
+++ src/main/java/org/wintaye/org/payroll/controllers/OrderController.java
@@ -1,49 +1,49 @@
-package org.wintaye.org.payroll.controllers;
-
-import org.springframework.hateoas.CollectionModel;
-import org.springframework.hateoas.EntityModel;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.wintaye.org.payroll.models.Order;
-import org.wintaye.org.payroll.models.Status;
-import org.wintaye.org.payroll.models.datamodels.OrderRepository;
-
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
-
-@RestController
-public class OrderController {
-
-    OrderRepository orderRepository;
-    OrderModelAssembler assembler;
-
-    OrderController(OrderRepository orderRepository, OrderModelAssembler assembler){
-        this.orderRepository = orderRepository;
-        this.assembler = assembler;
-    }
-
-    @GetMapping("/orders")
-    CollectionModel<EntityModel<Order>> all(){
-        List<EntityModel<Order>> orders = orderRepository.findAll().stream().map(assembler::toModel).collect(Collectors.toList());
-        return CollectionModel.of(orders, linkto(methodOn(OrderController.class).all)()).withSelfRel());
-    }
-
-    @GetMapping("/orders/{id}")
-    EntityModel<Order> one(@PathVariable Long id){
-        Order order = orderRepository.findById(id).orElseThrow(() -> new OrderNotFoundException(id));
-
-        return assembler.toModel(order);
-    }
-
-    @PostMapping("/orders")
-    ResponseEntity<EntityModel<Order>> newOrder(@RequestBody Order order){
-        order.setStatus(Status.IN_PROGRESS);
-        Order newOrder = orderRepository.save(order);
-
-        return ResponseEntity.created(linkTo(methodOn(OrderController.class).one(newOrder.getId())).toUri()).body(assembler.toModel(newOrder));
-    }
-
-
-
-}
+//package org.wintaye.org.payroll.controllers;
+//
+//import org.springframework.hateoas.CollectionModel;
+//import org.springframework.hateoas.EntityModel;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//import org.wintaye.org.payroll.models.Order;
+//import org.wintaye.org.payroll.models.Status;
+//import org.wintaye.org.payroll.models.datamodels.OrderRepository;
+//
+//import java.util.Collections;
+//import java.util.List;
+//import java.util.stream.Collectors;
+//
+//@RestController
+//public class OrderController {
+//
+//    OrderRepository orderRepository;
+//    OrderModelAssembler assembler;
+//
+//    OrderController(OrderRepository orderRepository, OrderModelAssembler assembler){
+//        this.orderRepository = orderRepository;
+//        this.assembler = assembler;
+//    }
+//
+//    @GetMapping("/orders")
+//    CollectionModel<EntityModel<Order>> all(){
+//        List<EntityModel<Order>> orders = orderRepository.findAll().stream().map(assembler::toModel).collect(Collectors.toList());
+//        return CollectionModel.of(orders, linkto(methodOn(OrderController.class).all)()).withSelfRel());
+//    }
+//
+//    @GetMapping("/orders/{id}")
+//    EntityModel<Order> one(@PathVariable Long id){
+//        Order order = orderRepository.findById(id).orElseThrow(() -> new OrderNotFoundException(id));
+//
+//        return assembler.toModel(order);
+//    }
+//
+//    @PostMapping("/orders")
+//    ResponseEntity<EntityModel<Order>> newOrder(@RequestBody Order order){
+//        order.setStatus(Status.IN_PROGRESS);
+//        Order newOrder = orderRepository.save(order);
+//
+//        return ResponseEntity.created(linkTo(methodOn(OrderController.class).one(newOrder.getId())).toUri()).body(assembler.toModel(newOrder));
+//    }
+//
+//
+//
+//}
